675313a18326f1b4da1843af6d6f13c8519b5088
Evan Epperson <evanepperson@pop-os.localdomain>
2021-03-04 21:58:16 -0600

    saving finished to do list

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { CredentialsContext } from "../App";
+import Todos from "../components/Todos";
+
 export default function Welcome() {
-	return (
-		<div>
-			<h1>Welcome</h1>
-			<Link to="/register">Register</Link>
-			<Link to="/login">Login</Link>
-		</div>
-	);
+  const [credentails, setCredentials] = useContext(CredentialsContext);
+  const logout = () => {
+    setCredentials(null);
+  };
+
+  return (
+    <div>
+      {credentails && <button onClick={logout}>Logout</button>}
+      <h1>Welcome {credentails && credentails.username}</h1>
+      {!credentails && <Link to="/register">Register</Link>}
+      <br />
+      {!credentails && <Link to="/login">Login</Link>}
+      {credentails && <Todos />}
+    </div>
+  );
 }
